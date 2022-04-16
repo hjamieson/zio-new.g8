bd5d90b9792c0f685c6fcdc91c717f81441df769
Hugh Jamieson <hjamieson5@gmail.com>
2022-04-16 14:50:50 -0400

    re-added description

--- src/main/g8/src/main/scala/$package__packaged$/Stub.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Stub.scala
@@ -5,7 +5,7 @@ package $package$
 
 object Stub {
   def main(args: Array[String]): Unit = {
-    println("$name$")
+    println("$description$")
   }
   // got any helpful boilerplate for your users?
 }
